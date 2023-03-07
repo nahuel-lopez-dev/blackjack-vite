aaa0d9274f1209bf56c1ecbd6993e5cc9ca9f7d0
Nahuel <nahuel.profesor@gmail.com>
2023-03-06 22:09:25 -0300

    working on refactoring

---
+++ src/blackjack/usecases/crear-deck.js
@@ -0,0 +1,23 @@
+import _ from "underscore";
+
+// Esta función crea un nuevo deck
+export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+
+    let deck = [];
+
+    for( let i = 2; i <= 10; i++ ) {
+        for( let tipo of tiposDeCarta ) {
+            deck.push( i + tipo);
+        }
+    }
+
+    for( let tipo of tiposDeCarta ) {
+        for( let esp of tiposEspeciales ) {
+            deck.push( esp + tipo);
+        }
+    }
+    // console.log( deck );
+    deck = _.shuffle( deck );
+    // console.log( deck );
+    return deck;
+}
